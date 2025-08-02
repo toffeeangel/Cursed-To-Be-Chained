e883c2881e26490b639f6bd546335788dfd8c6d4
Laleh Rehman <supershake.party671@passinbox.com>
2025-08-02 12:51:14 +0100

    Update MysticaReload.js

--- MysticaReload.js
+++ MysticaReload.js
@@ -1,22 +1,24 @@
 //Mystica: RELOAD
-//A text-based adventure game where the player will be able
-//to make choices that affect the outcome of the game.
+//A text-based choose-your-own-adventure mystery game wrapped in secrets, some too dangerous to tell...
 
+// Include readline for player input
 const readline = require("readline-sync");
 
+// Game stats
 let playerName = "";
 let playerHealth = 100;
-let playerCoin = 20;
-let currentLocation = "City";
+let playerCoin = 20; // Starting coins
+let currentLocation = "Miragem City";
 let gameRunning = true;
 let inventory = [];
 
 // Display the game title
 console.log("Welcome to Mystica: RELOAD");
 
-// Add a welcome message
+// Starting message
 console.log("Now entering uncharted territory...");
 
+// Welcome the player
 playerName = readline.question("\nWhat is your name, brave adventurer? ");
-console.log("\nWelcome, " + playerName + "!");
+console.log("\nWelcome, " + playerName + ".");
 console.log("\n You start with " + playerCoin + " coins.");
